7a24cebf0d4c09ca0765eabf7357d8176860b00e
Oleg <olegboz11111@gmail.com>
2024-10-18 14:05:41 +0300

    created homework15

---
+++ homework15.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+url = 'https://dummyjson.com/products?limit=200'
+
+response = requests.get(url)
+response_json = response.json()
+
+tech_products = [product['id'] for product in response_json['products'] if product.get('brand') == 'TechGear']
+print("ID продуктів бренду TechGear:", tech_products)
+
+product_id = 135
+product = next((product for product in response_json['products'] if product['id'] == product_id), None)
+
+if product:
+    image_url = product['thumbnail']
+
+    img_response = requests.get(image_url)
+
+    if img_response.status_code == 200:
+        with open('phone.png', 'wb') as img_file:
+            img_file.write(img_response.content)
+
+pass
